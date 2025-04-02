44f116b8cec3fdf4d871eca4797c66289546f22f
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2025-04-02 13:24:40 -0500

    .

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Define la URL base de tu API backend
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://carwash-backend-hrpl.onrender.com/api';
 
 // Crea una instancia de axios con la URL base
 const api = axios.create({
